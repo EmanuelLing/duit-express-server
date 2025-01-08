9ad882c3c798b52f781b864c47209f593a03ff94
yxiu5678 <yxiu5678@gmail.com>
2025-01-09 05:30:42 +0800

    Update backend for notification

--- server.js
+++ server.js
@@ -2,16 +2,41 @@
 const express = require('express');
 const pg = require('pg');
 const app = express();
+const cors = require('cors');
+
 
 // set up port
 const port = process.env.PORT || 3000;
 
+const adminRouter = require('./routes/admin.js');
+const notificationRouter = require('./routes/notification.js');
+
+const corsOptions = {
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+};
+
+app.use(cors(corsOptions)); 
+
 // parse json
-app.use(express.json());
+// Increase payload size limit to 10MB
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
+
+// Log request size
+app.use((req, res, next) => {
+  console.log(`Payload size: ${req.headers['content-length']} bytes`);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
+
+app.use('/admin',adminRouter);
+app.use('/notification',notificationRouter);
 
 // start up server
 app.listen(port, () => {
