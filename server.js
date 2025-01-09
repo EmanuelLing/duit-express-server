91f2a07685ec262b829fa1e941ab4ea0dc2f5675
ling <linghaoen@gmail.com>
2025-01-09 08:07:50 +0800

    budget, analysis, category and expense routes

--- server.js
+++ server.js
@@ -6,12 +6,22 @@ const app = express();
 // set up port
 const port = process.env.PORT || 3000;
 
+const categoryRouter = require('./routes/category');
+const budgetRouter = require('./routes/budget');
+const expenseRouter = require('./routes/expense');
+const analysisRouter = require('./routes/analysis');
+
 // parse json
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
+
+app.use('/category', categoryRouter);
+app.use('/budget', budgetRouter);
+app.use('/expense', expenseRouter);
+app.use('/analysis', analysisRouter);
 
 // start up server
 app.listen(port, () => {
